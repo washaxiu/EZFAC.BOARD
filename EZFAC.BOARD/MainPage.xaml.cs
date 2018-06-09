be486da0f378251a6a6feff76efd32311f1a5575
Administrator <Administrator@SC-201707200756>
2018-06-09 20:51:47 +0800

    添加项目文件。

---
+++ EZFAC.BOARD/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace EZFAC.BOARD
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Timer timer;
+        DispatcherTimer dispatcherTimer;
+
+        private SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        private SolidColorBrush gray = new SolidColorBrush(Colors.Gray);
+        private SolidColorBrush green = new SolidColorBrush(Colors.Green);
+        private int i;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            i = 0;
+            
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, object e)
+        {
+         
+        }
+
+        public void refresh(object state)
+        {
+            
+        }
+    }
+}
